7476f6bea90a183cbbe65eaefbf2c0055581fce5
yuanyi-350 <kysyy1@126.com>
2026-03-24 15:36:28 +0800

    init commit

--- lakefile.lean
+++ lakefile.lean
@@ -28,14 +28,8 @@ package «FoML» where
   moreServerOptions := linter
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "master"
+  "https://github.com/leanprover-community/mathlib4" @ "v4.29.0-rc6"
 
 @[default_target]
 lean_lib «FoML» where
   -- add any library configuration options here
-
-require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
-
-meta if get_config? env = some "dev" then
-require «doc-gen4» from git
-  "https://github.com/leanprover/doc-gen4" @ "main"
